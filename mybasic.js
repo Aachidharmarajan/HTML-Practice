d57d1b116bb576bea4b39d9a8d586ce5fc3d90ba
AACHI D <67627527+Aachidharmarajan@users.noreply.github.com>
2024-07-24 11:08:26 +0530

    Add files via upload

---
+++ mybasic.js
@@ -0,0 +1,6 @@
+function showImage() {
+    const img = document.getElementById('myImage');
+    img.src = 'https://www.pixelstalk.net/wp-content/uploads/2016/08/Background-Beautiful-Nature-Full-HD.jpg'; // Replace with your actual image URL
+    img.style.display = 'block';
+  alert("whohoooo")
+}
